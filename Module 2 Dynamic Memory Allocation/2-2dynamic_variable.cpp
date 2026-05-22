b49895186dc3ab8f0a1694fcd0227560abf7de0e
Your Name <muhammadTawhid>
2026-05-23 03:48:31 +0900

    Dynamic memory allocation

---
+++ Module 2 Dynamic Memory Allocation/2-2dynamic_variable.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int *p = new int;
+    *p = 12;
+    cout << *p;
+
+    return 0 ;
+}
---
+++ Module 2 Dynamic Memory Allocation/test.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout << "hello";
+    cout << "\n" << "world";
+    return 0;
+}
